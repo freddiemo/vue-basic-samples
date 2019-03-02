ad853abda0d56a59a573ce96288d4e9a3b2e79d9
gl <freddiejmo@gmail.com>
2019-03-02 19:13:39 -0400

    4 handling user input

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
     }
 });
 
